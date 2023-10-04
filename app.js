f56560a2ac139027ff8080b9fd16242373fcb37a
Chris Peterkin <christopherpeterkin@maximus.com>
2023-10-04 15:32:28 -0400

    conditional rendering

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const app = Vue.createApp({
     // template: "<h2>I am the template</h2>"
     data() {
         return {
+            showBooks: true,
             title: 'Ender\'s Game',
             author: 'Orson Scott Card',
             age: 47,
         }
     },
     methods: {
-        changeTitle(title) {
-            // this.title = 'Words of Radiance'
-            this.title = title
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
         }
     }
 })
