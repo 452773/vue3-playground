bbc53dc2549fafc3446b0624efe63110c9297f17
Chris Peterkin <christopherpeterkin@maximus.com>
2023-10-04 14:28:48 -0400

    adding initial Vue functionalities

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const app = Vue.createApp()
+const app = Vue.createApp({
+    // template: "<h2>I am the template</h2>"
+    data() {
+        return {
+            title: 'Ender\'s Game',
+            author: 'Orson Scott Card',
+            age: 47,
+        }
+    },
+    methods: {
+        changeTitle(title) {
+            // this.title = 'Words of Radiance'
+            this.title = title
+        }
+    }
+})
 
 app.mount('#app')
